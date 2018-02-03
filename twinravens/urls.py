e43fb13b641662190d9b75db147369cdefd5d56e
jkang8 <johnkang.h@gmail.com>
2018-02-03 15:53:15 -0800

    fix admin path

--- twinravens/urls.py
+++ twinravens/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
@@ -21,5 +20,5 @@ from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', index),
+    path('', index),
 ]
