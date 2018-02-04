b0b6af3fe46234950e99f1038277b1335e33deaf
Michael Fuery <mfuery@gmail.com>
2018-02-03 17:42:12 -0800

    Remove template in wrong place

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics, renderers, status
+from rest_framework import generics, renderers, status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
