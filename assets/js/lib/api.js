fea38224ec5682936bf0365acb19845d8fd8128c
Julian Threatt <porfirio84@gmail.com>
2018-02-03 18:27:27 -0800

    commit of first take of map component

---
+++ assets/js/lib/api.js
@@ -0,0 +1,15 @@
+export const Api = function(opts) {
+  opts = opts || {};
+
+  const apiKey = opts.apiKey;
+  const URL = '';
+
+  const url = () => {
+    let url = URL;
+    return `${url}`;
+  };
+
+  return url();
+};
+
+export default Api
---
+++ assets/js/views/map.jsx
@@ -0,0 +1,81 @@
+import React, {PropTypes as T} from 'react'
+import ReactDOM from 'react-dom'
+
+import cache from '../lib/ScriptCache'
+import Api from '../lib/api'
+
+const defaultMapConfig = {};
+export const wrapper = (options) => (WrappedComponent) => {
+  const apiKey = options.apiKey;
+
+  class Wrapper extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+
+      this.state = {
+        loaded: false,
+        map: null,
+        tomtom: null
+      };
+    }
+    componentDidMount() {
+      const refs = this.refs;
+      this.scriptCache.tomtom.onLoad((err, tag) => {
+        const tomtom = window.tomtom;
+        const props = Object.assign({}, this.props, {
+          loaded: this.state.loaded
+        });
+
+        const mapRef = refs.map;
+
+        const node = ReactDOM.findDOMNode(mapRef);
+        // center
+
+        // map config
+
+        tomtom.setProductInfo('Twin Ravens', '0.0.1');
+        this.map = tomtom.map(mapRef, {
+          key: apiKey,
+          center: [41, -96],  // America
+          zoom: 4,
+          layers: [
+            tomtom.L.tileLayer.wms(`https://api.tomtom.com/map/1/wms/?key=${apiKey}`, {
+              layers: 'basic',
+              format: 'image/jpeg'
+            })
+          ],
+          source: 'vector',
+          basePath: '/static'
+        });
+
+        this.setState({
+          loaded: true,
+          map: this.map,
+          tomtom: window.tomtom
+        });
+      });
+    }
+    componentWillMount() {
+      this.scriptCache = cache({
+        tomtom: Api({
+          apiKey: apiKey
+        })
+      });
+    }
+    render() {
+      const props = Object.assign({}, this.props, {
+        loaded: this.state.loaded,
+        map: this.state.map,
+        tomtom: this.state.tomtom,
+        mapComponent: this.refs.map
+      });
+      return <div>
+        <WrappedComponent {...props}/>
+        <div ref="map" />
+      </div>;
+    }
+  }
+  return Wrapper;
+};
+
+export default wrapper;
