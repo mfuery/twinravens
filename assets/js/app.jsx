24db1731c51f9080f80865a5cb5d8832a33d9070
Julian Threatt <porfirio84@gmail.com>
2018-02-04 12:01:04 -0800

    hacky map stuff

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -7,6 +7,7 @@ import TopNav from './views/topnav.jsx';
 import TripStore from './stores/tripStore';
 import wrapper from './views/map.jsx';
 import CreateTrip from "./views/createTrip.jsx";
+import moment from 'moment';
 
 
 class Card extends React.Component {
@@ -19,7 +20,7 @@ class Card extends React.Component {
             {/*<span class="card-title">Card Title</span>*/}
           </div>
           <div className="card-content">
-            <span className="card-title">{moment(this.props.date).format('DDD MMM YYYY')}</span>
+            <span className="card-title">{moment(this.props.date).format('MMMM Do YYYY, h:mm:ss a')}</span>
             <span className="card-title">{this.props.name}</span>
             <p>{this.props.description}</p>
           </div>
@@ -36,7 +37,17 @@ class Card extends React.Component {
 
 class Container extends React.Component {
   render() {
-    return <div></div>;
+    const cards = this.props.tripStore.stops;
+    return <div>
+      {cards.map(x => {
+        return <Card key={x.id}
+                     id={x.id}
+                     description={x.location.address}
+                     name={x.location.name}
+                     date={x.when}
+                     imageUrl={'http://www.developerweek.com/wp-content/uploads/2015/11/hackaton-banner-badge.png'} />;
+      })}
+    </div>;
   }
 }
 const TomTom = wrapper({apiKey: 'c26Y46QwvfTgsAirK4Nh0w8YokZJ3XGq'})(Container);
@@ -47,18 +58,38 @@ class App extends Listener {
     // set up loading state
     if (true) {
       // component routes here
+      const tripStore = this.state.model.state.tripStore;
+      const tripActions = this.state.model.actions.tripStore;
       return (<div className="app-container">
         <TopNav/>
         <Switch>
           <Route exact path={'/'} component={() => <Dash
-            tripStore={this.state.model.state.tripStore}
+            tripStore={tripStore}
           />}/>
           <Route exact path={'/create_trip'} component={() => <CreateTrip
-            tripStore={this.state.model.state.tripStore}
-            tripActions={this.state.model.actions.tripStore}
+            tripStore={tripStore}
+            tripActions={tripActions}
           />}/>
           <Route exact path={'/maptest'}
-                 component={() => <TomTom/>}
+                 component={() => {
+                   const stops = tripStore.stops;
+                   const first = stops[0];
+                   let center;
+                   if (first)
+                     center = {lat: first.location.lat, lng: first.location.lon};
+                   const locations = stops.map(x => {
+                     return {lat: x.location.lat, lng: x.location.lon};
+                   });
+                   // const locations = [
+                   //   {lat: 37.832619, lng: -122.480118}, // golden gate
+                   //   {lat: 37.802706, lng: -122.405904}, // coit
+                   //   {lat: 37.795403, lng: -122.393640}, // ferry
+                   //   {lat: 37.785650, lng: -122.401130}]; // moma
+                   // const locations = [{lat: 51.63685, lng: 19.4171}, {lat: 52.23498, lng: 21.00849}];
+                   return <TomTom tripStore={tripStore}
+                                  tripActions={tripActions}
+                                  center={center} locations={locations} />;
+                 }}
           />
           {/*<Route path={urls.ESTIMATES} component={() => <SampleCompoent*/}
           {/*/>}/>*/}
